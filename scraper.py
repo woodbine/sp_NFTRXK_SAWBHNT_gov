17415ada8955f96182c8f85a466fc2482cac92a3
blablupcom <o.volodin@yahoo.com>
2015-11-06 09:49:24 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,79 +2,113 @@ import requests
 from bs4 import BeautifulSoup as bs
 import scraperwiki
 from datetime import datetime
-from multiprocessing.dummy import Pool
-from multiprocessing import cpu_count
-import grequests
+import re
 
-start_urls = ['http://www.amazon.com/Best-Sellers-Toys-Games-Toy-Figures-Playsets/zgbs/toys-and-games/165993011/ref=zg_bs_nav_t_1_t', 
-'http://www.amazon.com/Best-Sellers-Toys-Games-Balls-Babies-Toddlers/zgbs/toys-and-games/2522032011/ref=zg_bs_nav_t_2_196601011', 
-'http://www.amazon.com/Best-Sellers-Toys-Games-Baby-Toddler/zgbs/toys-and-games/196601011/ref=zg_bs_nav_t_1_t']
-pool = Pool(cpu_count() * 20)
+start_url = 'http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords={}'
+ua = {'User-agent': 'Mozilla/5.0'}
 
-def scrape(response, **kwargs):
-        listing_soup = bs(response.text, 'lxml')
-        asin_nums = listing_soup.find_all('div', 'zg_itemImmersion')
-        for asin_num in asin_nums:
-            asin = ''
+
+def parse(search_term):
+    search_page = requests.get(start_url.format(search_term), headers=ua)
+    print start_url.format(search_term)
+    soup = bs(search_page.text, 'lxml')
+
+    search_rows = soup.find_all('li', 's-result-item celwidget')
+    for search_row in search_rows:
+        search_term = search_term
+        title = search_row.find('h2', 'a-size-medium a-color-null s-inline s-access-title a-text-normal').text.strip()
+        asin = search_row.find('a', 'a-link-normal s-access-detail-page  a-text-normal')['href'].split('dp/')[-1].split('/')[0]
+        pubdate = ''
+        try:
+            pubdate = search_row.find('div', 'a-row a-spacing-small').find('span', 'a-size-small a-color-secondary').text.strip()
+        except:
+            pass
+        author = ''
+        try:
+            author = search_row.find('div', 'a-row a-spacing-small').find('div', 'a-row a-spacing-none').text.strip().split('by')[-1].strip()
+        except:
+            pass
+        item_format = ''
+        try:
+            item_format = search_row.find('h3', 'a-size-small a-color-null s-inline  a-text-normal').text.strip()
+        except:
+            pass
+        price_rent = ''
+        try:
+            price_rent = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find(text=re.compile('to rent')).find_previous('span', 'a-size-base a-color-price s-price a-text-bold').text
+        except:
+            pass
+        price = ''
+        try:
+            price = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find(text=re.compile('to buy')).find_previous('span', 'a-size-base a-color-price s-price a-text-bold').text
+        except:
+            pass
+        low_price = ''
+        try:
+            low_price = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find(text=re.compile('used & new')).find_previous('span', 'a-size-base a-color-price a-text-bold').text
+        except:
+            pass
+        offer_count = ''
+        try:
+            offer_count = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find(text=re.compile('offers')).replace('(', '').replace(')','')
+        except:
+            pass
+        other1format = ''
+        try:
+            other1format = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find('hr', 'a-divider-normal s-result-divider').find_next('a').text
+        except:
+            pass
+        if other1format:
+            other1asin = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find('hr', 'a-divider-normal s-result-divider').find_next('a')['href'].split('dp/')[-1].split('/')[0]
+        else:
+            other1asin = ''
+        other2format = ''
+        try:
+            other2format = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find(text=re.compile('Other Format')).find_next('a').text
+        except:
+            pass
+        if other2format:
+            other2asin = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find(text=re.compile('Other Format')).find_next('a')['href'].split('dp/')[-1].split('/')[0]
+        else:
+            other2asin = ''
+        if other1format == other2format:
+            other1format = ''
+            other1asin = ''
+        comma = ''
+        try:
+            comma = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find(text=re.compile('Other Format')).find_next('a').find_next('span', 'a-size-small a-color-secondary').text
+        except:
+            pass
+        other3format = ''
+        if ',' in comma:
+            other3format = ''
             try:
-                asin = asin_num.find('a')['href'].split('dp/')[-1].strip()
+                other3format = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find(text=re.compile('Other Format')).find_next('a').find_next('a').text
             except:
                 pass
-            amazon_price = ''
-            try:
-                amazon_price = asin_num.find('strong', 'price').text.strip()
-            except:
-                pass
-            total_offer_count = ''
-            try:
-                total_offer_count = asin_num.find('div', 'zg_usedPrice').find('a').text.strip().split(u'\xa0')[0].replace('used & new', '')
-            except:
-                pass
-            lowest_price = ''
-            try:
-                lowest_price = asin_num.find('div', 'zg_usedPrice').find('span', 'price').text.strip()
-            except:
-                pass
-            today_date = str(datetime.now())
-            scraperwiki.sqlite.save(unique_keys=['Date'], data={'ASIN': asin, 'Date': today_date, 'Amazon Price': amazon_price, 'Total Offer Count': total_offer_count, 'Lowest Price': lowest_price})
-
-
-def multiparse(links):
-         l = links.find('a')['href'].encode('utf-8')
-         if l:
-             return l
-
-
-def parse(url):
-    if url in start_urls:
-        async_list = []
-        print (url)
-        for i in xrange(1, 6):
-            rs = (grequests.get(url+'?&pg={}'.format(i), hooks = {'response' : scrape}))
-            async_list.append(rs)
-        grequests.map(async_list)
-    page = requests.get(url)
-    soup = bs(page.text, 'lxml')
-    try:
-        active_sel = soup.find('span', 'zg_selected').find_next()
-        if active_sel.name == 'ul':
-            links_lists= active_sel.find_all('li')
-            asins = pool.map(multiparse, links_lists)
-            for asin in asins:
-                async_list = []
-                print (asin)
-                for i in xrange(1, 6):
-                    rs = (grequests.get(asin+'?&pg={}'.format(i), hooks = {'response' : scrape}))
-                    async_list.append(rs)
-                parse(asin)
-                grequests.map(async_list)
-
-
-    except:
-        parse(url)
-
+        if other3format:
+            other3asin = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find(text=re.compile('Other Format')).find_next('a').find_next('a')['href'].split('dp/')[-1].split('/')[0]
+        else:
+            other3asin = ''
+        newer = ''
+        try:
+            newer = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find('div', 'a-column a-span7').find('a', text=re.compile('See newer edition of this book'))
+        except:
+            pass
+        if newer:
+            newer = newer['href'].split('dp/')[-1].split('/')[0]
+        tradein = ''
+        try:
+            tradein = search_row.find('div', 'a-fixed-left-grid-col a-col-right').find(text=re.compile('Trade in yours for')).find_next('span', 'a-color-price').text
+        except:
+            pass
+        print title
+        today_date = str(datetime.now())
+        scraperwiki.sqlite.save(unique_keys=['Date'], data={'SearchString': search_term, 'Title': title, 'ASIN': asin, 'PubDate': pubdate, 'Author': author, 'Format': item_format, 'PriceRent': price_rent, 'Price': price, 'PriceLow': low_price, 'OfferCount': offer_count, 'Other1Format': other1format, 'Other1ASIN': other1asin, 'Other2Format': other2format, 'Other2ASIN': other2asin, 'Other3Format': other3format, 'Other3ASIN': other3asin, 'TradIn': tradein, 'Date': today_date})
 
 if __name__ == '__main__':
-
-   for start_url in start_urls:
-       parse( start_url)
+        file1 = open('amazon.txt', 'r')
+        for line in file1:
+            search_term = line.split('%')[-1]
+            search_term = '+'.join(search_term.split(' '))
+            parse(search_term)
