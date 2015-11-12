5c92f0cfd764d1a72c8c5b4daf5363b05eea53c4
blablupcom <o.volodin@yahoo.com>
2015-11-12 10:13:49 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -126,7 +126,7 @@ def parse(search_term, search_tag, p):
 
         # print title, asin, pubdate, author, item_format, price_rent, price, low_price, offer_count, other1format, other1asin, other2format, other2asin, other3format, other3asin, newer, tradein
         today_date = str(datetime.now())
-        scraperwiki.sqlite.save(unique_keys=['Date'], data={'SearchString': unicode(search_term), 'Search Tag': search_tag, 'Title': unicode(title), 'ASIN': asin, 'PubDate': pubdate, 'Author': unicode(author), 'Format': unicode(item_format), 'PriceRent': price_rent, 'Price': price, 'PriceLow': low_price, 'OfferCount': offer_count, 'Other1Format': unicode(other1format), 'Other1ASIN': other1asin, 'Other2Format': unicode(other2format), 'Other2ASIN': other2asin, 'Other3Format': unicode(other3format), 'Other3ASIN': other3asin, 'Newer_Edition': newer, 'TradeIn': unicode(tradein), 'Date': today_date})
+        scraperwiki.sqlite.save(unique_keys=['Date'], data={'SearchString': search_term.encode('utf-8'), 'Search Tag': search_tag.encode('utf-8'), 'Title': title.encode('utf-8'), 'ASIN': asin, 'PubDate': pubdate, 'Author': author.encode('utf-8'), 'Format': item_format.encode('utf-8'), 'PriceRent': price_rent.encode('utf-8'), 'Price': price.encode('utf-8'), 'PriceLow': low_price.encode('utf-8'), 'OfferCount': offer_count.encode('utf-8'), 'Other1Format': other1format.encode('utf-8'), 'Other1ASIN': other1asin, 'Other2Format': other2format.encode('utf-8'), 'Other2ASIN': other2asin, 'Other3Format': other3format.encode('utf-8'), 'Other3ASIN': other3asin, 'Newer_Edition': newer, 'TradeIn': tradein.encode('utf-8'), 'Date': today_date})
 
 
 if __name__ == '__main__':
